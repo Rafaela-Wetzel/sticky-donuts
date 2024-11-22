ff37a56715335f813abd5405064d70c3f2aa6218
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-11-22 21:30:04 +0000

    Update emailjs code

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,15 +1,15 @@
 // EmailJS Contact Service Code
 
 window.onload = function () {
-    document.getElementById('donut-contact').addEventListener('submit', function (event) {
+    document.getElementById('code_institute').addEventListener('submit', function (event) {
         event.preventDefault();
-        emailjs.sendForm('service_r17121989w', 'donut_template', this)
+        emailjs.sendForm('service_r17121989w', 'code_institute', this)
             .then(function () {
                 Swal.fire({
                     icon: "success",
                     text: "Thank you for your message! We will get in touch with you soon."
                 });
-                document.getElementById("donut-contact").reset();
+                document.getElementById("code_institute").reset();
             }, function (error) {
                 Swal.fire({
                     icon: "error",
